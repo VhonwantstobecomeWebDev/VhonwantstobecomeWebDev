df91291393ec28f7f2e52d4c2f734d946f39114d
VhonwantstobecomeWebDev <vhon.lampa@gmail.com>
2024-11-23 19:29:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+function toggleMenu() {
+    const menuItems = document.querySelector('.menu-items');
+    menuItems.classList.toggle('active');
+  }
+document.querySelector(".add-to-cart1").addEventListener("click", function() {
+    alert("Item succesfully added to your cart.");
+});
+
+document.querySelector(".buy-now1").addEventListener("click", function() {
+    alert("Item succesfully ordered");
+});
+document.querySelector(".add-to-cart2").addEventListener("click", function() {
+    alert("Item succesfully added to your cart.");
+});
+
+document.querySelector(".buy-now2").addEventListener("click", function() {
+    alert("Item succesfully ordered");
+});
+document.querySelector(".add-to-cart3").addEventListener("click", function() {
+    alert("Item succesfully added to your cart.");
+});
+
+document.querySelector(".buy-now3").addEventListener("click", function() {
+    alert("Item succesfully ordered");
+});
+document.querySelector(".add-to-cart4").addEventListener("click", function() {
+    alert("Item succesfully added to your cart.");
+});
+
+document.querySelector(".buy-now4").addEventListener("click", function() {
+    alert("Item succesfully ordered");
+});
+document.querySelector(".add-to-cart5").addEventListener("click", function() {
+    alert("Item succesfully added to your cart.");
+});
+
+document.querySelector(".buy-now5").addEventListener("click", function() {
+    alert("Item succesfully ordered");
+});
+document.querySelector(".add-to-cart6").addEventListener("click", function() {
+    alert("Item succesfully added to your cart.");
+});
+
+document.querySelector(".buy-now6").addEventListener("click", function() {
+    alert("Item succesfully ordered");
+});
+document.querySelector(".add-to-cart7").addEventListener("click", function() {
+    alert("Item succesfully added to your cart.");
+});
+
+document.querySelector(".buy-now7").addEventListener("click", function() {
+    alert("Item succesfully ordered");
+});
+document.querySelector(".add-to-cart8").addEventListener("click", function() {
+    alert("Item succesfully added to your cart.");
+});
+
+document.querySelector(".buy-now8").addEventListener("click", function() {
+    alert("Item succesfully ordered");
+});
+document.querySelector(".add-to-cart9").addEventListener("click", function() {
+    alert("Item succesfully added to your cart.");
+});
+
+document.querySelector(".buy-now9").addEventListener("click", function() {
+    alert("Item succesfully ordered");
+});
+document.querySelector(".add-to-cart10").addEventListener("click", function() {
+    alert("Item succesfully added to your cart.");
+});
+
+document.querySelector(".buy-now10").addEventListener("click", function() {
+    alert("Item succesfully ordered");
+});
+document.querySelector(".add-to-cart11").addEventListener("click", function() {
+    alert("Item succesfully added to your cart.");
+});
+
+document.querySelector(".buy-now11").addEventListener("click", function() {
+    alert("Item succesfully ordered");
+});
+document.querySelector(".add-to-cart12").addEventListener("click", function() {
+    alert("Item succesfully added to your cart.");
+});
+
+document.querySelector(".buy-now12").addEventListener("click", function() {
+    alert("Item succesfully ordered");
+});
